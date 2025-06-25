568834278f40843961cc18c3795b59a17e500ab8
harsh <harshhate5@gmail.com>
2025-06-25 17:50:35 +0530

    26

---
+++ JS5/alert.js
@@ -0,0 +1,20 @@
+//Does't return anything
+//It is to alert the user/customer
+
+// alert("Download complete")
+
+//PROMPT
+// let prom = prompt("Enter your name", "Name");
+// console.log(prom);
+
+//CONFIRM
+let deletePost = confirm("Do you really want to delete this post?")
+
+
+if(deletePost===true){
+    console.log("Harsh is smart");
+}
+else{
+    console.log("Krishiv is funny");
+}
+console.log(deletePost);
