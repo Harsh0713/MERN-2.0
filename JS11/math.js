e4b0f998af6b0b954f1f8be60dd595768005f54d
harsh <harshhate5@gmail.com>
2025-06-26 17:03:35 +0530

    33

---
+++ JS11/math.js
@@ -0,0 +1,55 @@
+let mathobj = Math;
+console.log(mathobj);
+
+
+//Euler Number
+console.log("The value of Math.E is", Math.E);
+console.log("Math.Pi", Math.PI);
+
+
+console.log("LN2: ", Math.LN2)
+
+//Square Root of number
+console.log("Square Root: ", Math.SQRT1_2)
+
+//Simple addition
+let a=2.85345
+let b=23
+console.log("The value of a and b is: ",a+b);
+
+//Round off the number
+console.log("The round off of a and b is:", Math.round(a),Math.round(b))
+
+//Printing the powered number
+console.log("4 raised to the power of 2 is", Math.pow(4,2))
+
+//Square root of number
+console.log('Square root of a number', Math.sqrt(100));
+
+//Rounding up ceil and floor
+console.log("5.8 rounded up to the nearest integer is ",Math.ceil(5.9))
+console.log("5.8 rounded down to the nearest integer is ",Math.floor(5.9))
+
+//Find absolute of a number
+console.log("The absolute of a number", Math.abs(-8.456))
+console.log("The absolute of a number", Math.abs(19.456))
+
+//Trignometric functions
+console.log("The value of sin(pi/2) is:", Math.sin(Math.PI/2) )
+console.log("The value of tan(pi/2) is:", Math.tan(Math.PI/2) )
+console.log("The value of cos(pi/2) is:", Math.cos(Math.PI/2) )
+
+//Min Max
+console.log("Minimum of 12,23,54,65,11 is: ", Math.min(12,23,54,65,11))
+console.log("Maximum of 12,23,54,65,11 is: ", Math.max(12,23,54,65,11))
+
+//Generating the random value
+console.log("Generating the random value:", Math.random())
+
+//from 1-100
+let minNum =1;
+let maxNum = 100;
+let random = minNum+(maxNum-minNum)*Math.random();
+console.log(random)
+
+
